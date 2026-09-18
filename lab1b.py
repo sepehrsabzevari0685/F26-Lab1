52c415dff3ddd6145f8f9cc740d47eb8bb023860
sepehrsabzevari0685 <sepehrsabzevari1385@gmail.com>
2026-09-18 16:54:34 -0400

    Update lab1b.py

--- lab1b.py
+++ lab1b.py
@@ -20,15 +20,15 @@
 
 
 num1 = input("enter first number:")
-num2 = input("enter second number")
+num2 = input("enter second number:")
 
 num1 = int(num1)
 num2 = int(num2)
 
 print("num1 + num2 =", num1 + num2)
-print("num1 - num2 =", num1 + num2)
-print("num1 * num2 =", num1 + num2)
-print("num1 ** num2 =", num1 + num2)
-print("num1 / num2 =", num1 + num2)
-print("num1 // num2 =", num1 + num2)
-print("num1 % num2 =", num1 + num2)
+print("num1 - num2 =", num1 - num2)
+print("num1 * num2 =", num1 * num2)
+print("num1 ** num2 =", num1 ** num2)
+print("num1 / num2 =", num1 / num2)
+print("num1 // num2 =", num1 // num2)
+print("num1 % num2 =", num1 % num2)
