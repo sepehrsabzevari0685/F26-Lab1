39497038ab5ae139acf154578a1c2dfe1dcb41ef
sepehrsabzevari0685 <sepehrsabzevari1385@gmail.com>
2026-09-18 16:46:37 -0400

    Update lab1d.py

--- lab1d.py
+++ lab1d.py
@@ -24,3 +24,24 @@
 #TO-DO 4:
 # Use slicing to retun everything between index 2-15 to the user.
 # Print "uick brown foxs ju" from "words".
+
+
+
+name = "Sepehr"
+name = name.upper()
+
+age = 20
+
+print("How are you {}? Happy {}th birthday!".format(name, age))
+
+words = "The quick brown fox jumps over the lazy dog"
+
+print(words[0])
+print(words[16])
+
+print(words[-23:-18])
+print(words[-39:-34])
+
+print(words[2:15])
+
+print("quick brown fox ju" )
